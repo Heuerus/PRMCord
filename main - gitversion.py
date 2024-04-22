fe590a7d6b15cfac18791c2c73d64a5ee9c5b324
Jan Heuer <heuerus@gmail.com>
2024-04-22 09:38:15 +0200

    added env to bot, added requirements

--- main - gitversion.py
+++ main - gitversion.py
@@ -5,25 +5,28 @@ from random import randint
 import asyncio
 import settings
 import math
+from dotenv import load_dotenv
+import os
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+load_dotenv()
 
 @client.event
 async def on_ready():
     print("We are ready")
     print("-------")
 
-
-db = pymysql.connect(host = "--", user = "--", password = "--", database = "--", port = --)
+db = pymysql.connect(host = os.getenv("DB_host"), user = os.getenv("DB_user"), password = os.getenv("DB_password"), database = os.getenv("DB_database"), port = int(os.getenv("DB_port")))
 cursor = db.cursor()
 
-allowed_channel_id = [1211285687714971679,1211307699107536926,1211296263115636766,1215689387342434366,1215802619709362217]
+allowed_channel_id = [1211285687714971679,1211307699107536926,1211296263115636766,1215689387342434366,1215802619709362217, 714466799860580362]
 
 AceChannel = [1215774155140632618,1215774179144765510,1215774203987361994,1215774294055976970,1215775381588545556,1216782260594937947]
 
 allowed_channel_id = allowed_channel_id + AceChannel
 
-q_channel = [1215689387342434366,1215774179144765510]
+q_channel = [1215689387342434366,1215774179144765510, 714466799860580362]
 
 client.remove_command("help")
 
@@ -832,7 +835,7 @@ async def randomize_team(ctx):
 @client.command(name="refill")
 async def refill(ctx):  
     user_id = ctx.author.id
-    if user_id == 284347406420803596 or user_id == 574953391705292801:
+    if user_id == 284347406420803596 or user_id == 574953391705292801 or user_id == 262646553506873345:
         cursor.execute("UPDATE fantasy SET pulls = 10")
         db.commit()
         cursor.execute("UPDATE fantasy SET pulls = pulls + 5 ORDER BY elo DESC LIMIT 5")
@@ -1858,4 +1861,4 @@ async def promote(ctx, zustand=None):
     await ctx.send("Herzlich Willkommen... Am Anfang?")
 
 
-client.run("--")
+client.run(os.getenv("DC_token"))
